14b79a6d9f26a06729936945e031a56c862c43d4
Narendra Dantala <narendra417@gmail.com>
2020-07-25 11:47:41 -0400

    updated with initial commit

---
+++ src/main/java/com/wawa/hcl/workloadmanagement/WorkloadmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.wawa.hcl.workloadmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloadmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkloadmanagementApplication.class, args);
+	}
+
+}
