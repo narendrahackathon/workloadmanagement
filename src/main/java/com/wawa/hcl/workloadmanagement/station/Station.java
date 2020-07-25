0b468610ba8a00f20b23e3e1dd47e2580b4e4962
Narendra Dantala <narendra417@gmail.com>
2020-07-25 16:14:26 -0400

    updated

--- src/main/java/com/wawa/hcl/workloadmanagement/station/Station.java
+++ src/main/java/com/wawa/hcl/workloadmanagement/station/Station.java
@@ -1,7 +1,5 @@
 package com.wawa.hcl.workloadmanagement.station;
 
-import com.wawa.hcl.workloadmanagement.entity.Item;
-
 public interface Station {
 
 	public void process(Item item);
--- src/main/java/com/wawa/hcl/workloadmanagement/station/StationImpl.java
+++ src/main/java/com/wawa/hcl/workloadmanagement/station/StationImpl.java
@@ -1,7 +1,5 @@
 package com.wawa.hcl.workloadmanagement.station;
 
-import com.wawa.hcl.workloadmanagement.entity.Item;
-
 public class StationImpl implements Station {
 
 	@Override
